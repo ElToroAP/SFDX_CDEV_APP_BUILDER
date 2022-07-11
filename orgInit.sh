ca89ead153ceac1882fd4435f962cb470b3525f6
Andres Perez <eltoroit@users.noreply.github.com>
2022-07-11 23:17:13 +0200

    Update orgInit.sh

--- orgInit.sh
+++ orgInit.sh
@@ -1,3 +1,3 @@
-sfdx force:org:create --definitionfile="config/project-scratch-def.json" --durationdays=30 --setalias="${soALIAS}" --targetdevhubusername="${dhALIAS}" --json
+sfdx force:org:create --definitionfile="config/project-scratch-def.json" --durationdays=30 --setalias="${soALIAS}" --targetdevhubusername="${dhALIAS}" --wait=30 --json
 sfdx force:org:open --targetusername="${soALIAS}"
 sfdx force:source:push --targetusername="${soALIAS}"
